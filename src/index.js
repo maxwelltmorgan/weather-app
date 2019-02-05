331491a34910f3011b5baa9c1f3f897c13697a77
Maxwell Morgan <maxwelltmorgan@gmail.com>
2019-02-05 00:40:54 -0700

    css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './style/Current.css';
-import './style/FiveDay.css';
-import './style/Form.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
